f9ea4bace40d099cdda88dc82553ad1d0f818a9c
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-04-22 18:15:09 -0300

    feat: challange page

---
+++ lib/shared/widgets/progress_indicator/progress_indicator_widget.dart
@@ -0,0 +1,15 @@
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final double value;
+  const ProgressIndicatorWidget({Key key, this.value}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+        value: 0.3,
+        backgroundColor: AppColors.chartSecondary,
+        valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary));
+  }
+}
